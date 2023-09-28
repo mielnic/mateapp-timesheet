841ed2719b248234c5bc9832c3f05589db28671d
Paulo Mielnichuk <paulo@mielnichuk.com.ar>
2023-09-28 12:01:23 -0300

    Safari future date timesheet validator fix

--- mateapptimesheet/timesheet/validators.py
+++ mateapptimesheet/timesheet/validators.py
@@ -1,11 +1,11 @@
 from django.core.exceptions import ValidationError
-from datetime import date
+from datetime import date, timedelta
 from django.utils.translation import gettext_lazy as _
 
-today = date.today()
+tomorrow = date.today() + timedelta(days=1)
 
 def invalidate_future_timesheets(timeDate):
-    if timeDate > today:
+    if timeDate >= tomorrow:
         raise ValidationError(_("You can't log timesheets in a future date."))
     else:
         return timeDate
